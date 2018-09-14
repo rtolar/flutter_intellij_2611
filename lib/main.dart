32c0b3d0229f00aedff6a68a3e2fe9921b7bb3cd
Richard Tolar <rtolar@emporiaenergy.com>
2018-09-14 12:15:48 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+  // var client = http.C
+}
+
